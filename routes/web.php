d60304024495d708e5f11c670d7500b8168b0a5f
Ali Haider <alihaider47125@gmail.com>
2026-05-03 09:18:55 +0500

    chore: add debug route. Update .env.example

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Alihaiderx\LaravelSpool\Controllers\TestController;
+use Alihaiderx\LaravelSpool\Middleware\EnsureDevEnv;
 use Illuminate\Support\Facades\Route;
 
-
-
-?>
+Route::middleware([EnsureDevEnv::class])->group(function () {
+  Route::get('/spool/test/debug', [TestController::class, 'debugRequest']);
+});
--- src/Middleware/EnsureDevEnv.php
+++ src/Middleware/EnsureDevEnv.php
@@ -5,15 +5,18 @@ namespace Alihaiderx\LaravelSpool\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class EnsureDevEnv {
+class EnsureDevEnv
+{
 
-  public function handle(Request $request, Closure $next){
-    
+  public function handle(Request $request, Closure $next)
+  {
+
+    $envs = ['local', 'dev', 'development'];
+
+    if (!in_array(strtolower(config('app.env')), $envs)) {
+      abort(401);
+    }
+
     return $next($request);
-
   }
-
 }
-
-
-?>
